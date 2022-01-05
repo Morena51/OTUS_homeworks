902302d2bfce727f808c6c36e153c382125c5e89
Marina Petrova <mnpetrova@sbercloud.ru>
2022-01-05 21:26:28 +0300

    Rename coverage to coverage_app

--- homework_07/coverage/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Project, APITest, E2ETest, UnitTest
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(APITest)
-admin.site.register(E2ETest)
-admin.site.register(UnitTest)
