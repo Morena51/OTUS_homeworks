7ced10fc119f4c61144b7631512d592d5fce7678
Marina Petrova <mnpetrova@sbercloud.ru>
2022-01-12 21:23:44 +0300

    Add tests

--- homework_07/coverage_app/app/apps.py
+++ homework_07/coverage_app/app/apps.py
@@ -1,6 +1,27 @@
-from django.apps import AppConfig
+from django.test import TestCase
+from
 
 
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
+class ProjectTest(TestCase):
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project = {
+            'name': 'testing'
+        }
+
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+
+    def test_create_project(self):
+        response = self.client.post(
+            '/zauth/user/create/',
+            data=self.user_data
+        )
+        self.assertEqual(302, response.status_code)
+
+
+    def test_list_project(self):
+        pass
